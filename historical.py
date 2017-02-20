b60184e6ac37c9ae6c04bf854589cd4bcba2c5ee
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-02-20 16:28:20 -0500

    add historical data code

---
+++ historical.py
@@ -0,0 +1,28 @@
+from yahoo_finance import Share
+from pprint import pprint
+
+stock_symbols = ['GOOGL', 'YHOO']
+
+def main():
+    for symbol in stock_symbols:
+        print 'getting data for', symbol
+        stock = Share(symbol)
+        data = stock.get_historical('2014-01-01', '2016-01-01')
+        
+        path = 'data/hist_' + symbol + '.csv'
+        with open(path, 'w') as f:
+            for day_data in data:
+                line = []
+                line.append(day_data['Date'])
+                line.append(day_data['Open'])
+                line.append(day_data['High'])
+                line.append(day_data['Low'])
+                line.append(day_data['Close'])
+                line.append(day_data['Volume'])
+
+                f.write(', '.join(line) + '\n')
+
+        print 'data written to', path
+
+if __name__ == '__main__':
+    main()
