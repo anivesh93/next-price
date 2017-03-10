bb31beeae8745cb20dc992bcb0e9b79ae8b0d4ae
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-03-09 19:38:46 -0500

    modify to store to db

--- historical.py
+++ historical.py
@@ -1,29 +1,35 @@
 from yahoo_finance import Share
 from pprint import pprint
 
+import sqlite3
+
 stock_symbols = ['GOOGL', 'YHOO', 'MSFT', 'AMZN', 'TWTR', \
         'FB', 'CSCO', 'BAC', 'AAPL', 'AMD']
 
 def main():
+
+    conn = sqlite3.connect('stocks.db')
+    cursor = conn.cursor()
+    insert_query = \
+        'INSERT INTO historical VALUES ("{0}", {1}, {2}, {3}, {4}, "{5}", "{6}")'
+
     for symbol in stock_symbols:
         print 'getting data for', symbol
         stock = Share(symbol)
         data = stock.get_historical('2014-01-01', '2016-01-01')
         
         path = 'data/hist_' + symbol + '.csv'
-        with open(path, 'w') as f:
-            for day_data in data:
-                line = []
-                line.append(day_data['Date'])
-                line.append(day_data['Open'])
-                line.append(day_data['High'])
-                line.append(day_data['Low'])
-                line.append(day_data['Close'])
-                line.append(day_data['Volume'])
+        for day_data in data:
+            cursor.execute(insert_query.format(
+                symbol,
+                day_data['Open'],
+                day_data['High'],
+                day_data['Low'],
+                day_data['Close'],
+                day_data['Date'],
+                day_data['Volume']))
 
-                f.write(', '.join(line) + '\n')
-
-        print 'data written to', path
+        conn.commit()
 
 if __name__ == '__main__':
     main()
