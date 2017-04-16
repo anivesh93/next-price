af79b3c9924a955400aa23cb6140d2fc9823f6ba
anives93 <anivesh93@gmail.com>
2017-04-16 02:06:27 -0400

    added plot.html

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import json
+import sqlite3
 from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
@@ -23,6 +25,34 @@ def stock(symbol=None):
 def hello(name=None):
     name = {1: 'foo', 2: 'bar', 3: 'baz'}
     return render_template('hello.html', name=name)
+
+@app.route('/plot/')
+def plot():
+	conn = sqlite3.connect('data/stocks.db')
+	cursor = conn.cursor()
+
+	cursor.execute('SELECT * FROM historical LIMIT 50')
+	rows = cursor.fetchall()
+
+	cleaned = []
+	ctr = 1
+
+	for row in rows:
+	    # print row[4], row[5]
+	    temp = {}
+	    temp["closePrice"]  = row[4]
+	    temp["date"] = row[5]
+	    cleaned.append(temp)
+	    ctr += 1
+
+	for row in cleaned:
+	    print row
+
+	conn.close()
+
+	fp = open('templates/data.json', 'wb')
+	json.dump(cleaned, fp, indent = 3)
+	return render_template('plot.html')
 
 def main():
     app.run()
