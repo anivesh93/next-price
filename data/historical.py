93107c2e42f4651ab6aae328f0a4df461d2fac44
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-04-18 16:34:56 -0400

    Repopulate historical table

--- data/historical.py
+++ data/historical.py
@@ -11,25 +11,27 @@ def main():
     conn = sqlite3.connect('stocks.db')
     cursor = conn.cursor()
     insert_query = \
-        'INSERT INTO historical VALUES ("{0}", {1}, {2}, {3}, {4}, "{5}", "{6}")'
+        'INSERT INTO historical VALUES ("{0}", "{1}", {2}, {3}, {4}, {5}, {6})'
 
     for symbol in stock_symbols:
         print 'getting data for', symbol
         stock = Share(symbol)
-        data = stock.get_historical('2014-01-01', '2016-01-01')
+        data = stock.get_historical('2014-01-01', '2017-04-18')
         
         path = 'data/hist_' + symbol + '.csv'
         for day_data in data:
             cursor.execute(insert_query.format(
                 symbol,
+                day_data['Date'],
                 day_data['Open'],
                 day_data['High'],
                 day_data['Low'],
                 day_data['Close'],
-                day_data['Date'],
                 day_data['Volume']))
 
         conn.commit()
+
+    conn.close()
 
 if __name__ == '__main__':
     main()
