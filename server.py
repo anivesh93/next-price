0bc12928c30632f43c228409c737f28699f12ccc
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-04-18 15:06:04 -0400

    Fix previous pull

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-import json
-import sqlite3
 from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
@@ -21,35 +19,6 @@ def historical(symbol=None):
 def hello(name=None):
     name = {1: 'foo', 2: 'bar', 3: 'baz'}
     return render_template('hello.html', name=name)
-
-@app.route('/plot/')
-@app.route('/plot/<name>')
-def plot(name=None):
-	conn = sqlite3.connect('data/stocks.db')
-	cursor = conn.cursor()
-
-	cursor.execute('SELECT * FROM historical LIMIT 50')
-	rows = cursor.fetchall()
-
-	cleaned = []
-	ctr = 1
-
-	for row in rows:
-	    # print row[4], row[5]
-	    temp = {}
-	    temp["closePrice"]  = row[4]
-	    temp["date"] = row[5]
-	    cleaned.append(temp)
-	    ctr += 1
-
-	# for row in cleaned:
-	    # print row
-
-	conn.close()
-
-	# fp = open('templates/data.json', 'wb')
-	# json.dump(cleaned, fp, indent = 3)
-	return render_template('plot.html', name = cleaned)
 
 def main():
     app.run()
