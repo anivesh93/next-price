afac71137306c8b19be531b62a7f556cfba5db4d
anives93 <anivesh93@gmail.com>
2017-04-19 00:34:29 -0400

    added line chart

--- server.py
+++ server.py
@@ -55,6 +55,68 @@ def plot(name=None):
 	# json.dump(cleaned, fp, indent = 3)
 	return render_template('plot.html', name = cleaned)
 
+@app.route('/lineplot2/')
+@app.route('/lineplot2/<name>')
+def lineplot2(name=None):
+	conn = sqlite3.connect('data/stocks.db')
+	cursor = conn.cursor()
+
+	cursor.execute('SELECT * FROM historical LIMIT 50')
+	rows = cursor.fetchall()
+
+	cleaned = []
+	ctr = 1
+
+	for row in rows:
+	    # print row[4], row[5]
+	    temp = {}
+	    temp["closePrice"]  = row[4]
+	    temp["date"] = row[5]
+	    cleaned.append(temp)
+	    ctr += 1
+
+	conn.close()
+
+	# data = [
+ #    {"year" : "2005", "closePrice": 770000},
+ #    {"year" : "2006", "closePrice": 670000},
+ #    {"year" : "2007", "closePrice": 570000},
+ #    {"year" : "2008", "closePrice": 770000},
+ #    {"year" : "2009", "closePrice": 870000}
+	# ]
+	# data = [
+ #    {"year" : "2005-10-10", "value": 870},
+ #    {"year" : "2006-11-11", "value": 770},
+ #    {"year" : "2010-12-12", "value": 570},
+ #    {"year" : "2012-09-09", "value": 740},
+ #    {"year" : "2015-08-08", "value": 780}
+	# ]
+	# return render_template('lineplot2.html', name = data)
+	return render_template('lineplot2.html', name = cleaned)
+
+@app.route('/googleChart')
+@app.route('/googleChart/<name>')
+def googleChart(name=None):
+	conn = sqlite3.connect('data/stocks.db')
+	cursor = conn.cursor()
+
+	cursor.execute('SELECT * FROM historical LIMIT 50')
+	rows = cursor.fetchall()
+
+	cleaned = []
+	ctr = 1
+
+	for row in rows:
+	    temp = []
+	    temp.append(row[5])
+	    temp.append(row[4])
+	    cleaned.append(temp)
+	    ctr += 1
+
+	conn.close()
+	
+	return render_template('googleChart.html', name = cleaned)
+
 def main():
     app.run()
 
