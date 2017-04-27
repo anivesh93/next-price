16a5b71f1d49f8954b9a8f972ca4aef34028116c
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-04-27 18:43:40 -0400

    Add comments for data python files

--- data/historical.py
+++ data/historical.py
@@ -7,7 +7,10 @@ import sqlite3
 stock_symbols = ['GOOGL', 'YHOO', 'MSFT', 'AMZN', 'TWTR', \
         'FB', 'CSCO', 'BAC', 'AAPL', 'AMD']
 
+# insert data function inserts data into the historical table
 def insert_data(symbol, prefix=""):
+
+    # open the database connection
     conn = sqlite3.connect(prefix + 'stocks.db')
     cursor = conn.cursor()
     insert_query = \
@@ -18,12 +21,15 @@ def insert_data(symbol, prefix=""):
     date_output_format = '%Y-%m-%d'
     date_output = datetime.now().strftime(date_output_format)
 
+    # call the api for historical data
     stock = Share(symbol)
     data = stock.get_historical('2014-01-01', date_output)
     
+    # iterate the data
     path = 'data/hist_' + symbol + '.csv'
     for day_data in data:
         try:
+            # insert into the database
             cursor.execute(insert_query.format(
                 symbol,
                 day_data['Date'],
@@ -36,11 +42,13 @@ def insert_data(symbol, prefix=""):
             print e
             print 'insert failed for', symbol, day_data['Date']
             
+    # commit and close the database
     conn.commit()
     conn.close()
 
 
 def main():
+    # go through all the symbols and get historical data for it
     for symbol in stock_symbols:
         insert_data(symbol)
 
--- data/init_db.py
+++ data/init_db.py
@@ -1,5 +1,7 @@
 import sqlite3 as sql
 
+
+# get all the queries from the db_commands.txt file
 def get_queries():
     with open('db_commands.txt', 'r') as q_file:
         content = q_file.read()
@@ -7,11 +9,14 @@ def get_queries():
 
 def main():
 
+    # open the connection to the database
     conn = sql.connect('stocks.db')
     cursor = conn.cursor()
 
-    cursor.executescript(get_queries())
+    # execute all the queries that are present in the db_commands.txt file
+    cursor.executescript(rget_queries())
     
+    # commit and close the connection
     conn.commit()
     conn.close()
 
